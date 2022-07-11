e8125230a04ac2a5237b1de03a0694423607c1a4
Ирина <bojarchukova@mail.ru>
2022-07-11 20:37:47 +0800

    Написать программу которая будет приветствовать пользователя по разному

---
+++ Example006_IfELSe/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу, которая будет приветствовать пользователя по разному
+Console.WriteLine("Введите имя пользователя");
+String username=Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
